192461daa5168741b5f6bdc7c955f5a565ac4fdf
TulyaRU <121633518+TulyaRU@users.noreply.github.com>
2023-01-08 21:36:41 +0600

    Create MainActivity.kt

---
+++ Activity open new activity/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnNextActivity = findViewById<Button>(R.id.btnNextActivity)
+        btnNextActivity.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
