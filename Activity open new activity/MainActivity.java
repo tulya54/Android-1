8677802cf587160fc9370c248cc47941db662c61
Tulya <33752668+tulya54@users.noreply.github.com>
2023-01-18 23:59:38 +0600

    Update MainActivity.java

--- Activity open new activity/MainActivity.java
+++ Activity open new activity/MainActivity.java
@@ -17,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, SecondActivity.class);
             startActivity(intent);
         });
+        //.лох
     }
 }
